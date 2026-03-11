c3360cc0c98999f59fa25c84ca58de0372fb3246
Rohan Deo <rohandeo2609@gmail.com>
2026-03-11 12:13:15 +0530

    Update Index.tsx

--- readmegen-frontend/src/pages/Index.tsx
+++ readmegen-frontend/src/pages/Index.tsx
@@ -23,7 +23,7 @@ const Index = () => {
     setMarkdown("");
 
     try {
-      const response = await fetch("https://gitglow-backend.onrender.com", {
+      const response = await fetch("https://gitglow-backend.onrender.com/generate-readme", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
